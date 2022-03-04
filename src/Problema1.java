b291a4d6bda4037afbef1b5893a2951f12c7c254
Álvaro Nicolás Ramos Zegarra <alvaron.ramoszegarra@iesfm.onmicrosoft.com>
2022-03-04 10:10:37 +0100

    Problema 1

--- src/Problema1.java
+++ src/Problema1.java
@@ -3,7 +3,15 @@ public class Problema1 {
     private int cadena_de_numeros() {
         int result = 0;
         for (int i = 0; i < 20; i++) {
-            i = result++;
+            if (es_multiplo_de_3(result)){
+                String name = "Fizz";
+
+            }else if (es_multiplo_de_5(result)){
+                String name = "Buzz";
+
+            }else if (es_multiplo_de_3(result) && es_multiplo_de_5(result)){
+                String name = "FizzBuzz";
+            }
         }
         return result;
     }
@@ -16,9 +24,6 @@ public class Problema1 {
         return n % 5 == 0;
     }
 
-    private boolean es_multiplo_de_3_y_5(Integer n) {
-        return n % 5 == 0;
-    }
-    
+
 
 }
